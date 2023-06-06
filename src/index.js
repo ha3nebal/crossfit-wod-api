6dc90a502d5d481f1293046870472b6e7449fb8c
Anibal Allendes <anibalallendesaravena@gmail.com>
2023-06-05 22:24:53 -0400

    "este si"

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require ("express");
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 
 const app = express ();
-const PORT = process.env.PORT || 3120; 
+const PORT = process.env.PORT || 3420; 
 
 app.use(express.json ());
 app.use("/api/v1/workouts", v1WorkoutRouter);
